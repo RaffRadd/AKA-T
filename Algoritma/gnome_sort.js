022c523f9ea23f6d2f856a6a708acbbe3a6285ca
RaffRadd <75944037+RaffRadd@users.noreply.github.com>
2020-12-26 18:33:04 +0700

    Typo Fix

--- Algoritma/gnome_sort.js
+++ Algoritma/gnome_sort.js
@@ -1,14 +1,15 @@
 function gnomeSort(arr) {
-	function moveBack(i) {
-    for (; i > 0 && arr[i - 1] > arr[i]; i--) {
-        var t = arr[i];
-		arr[i] = arr[i - 1];
-		arr[i - 1] = t;
-    }
-  }
-  for (i = 1; i < arr.length; i++) {
-    if (arr[i - 1] > arr[i])
-      moveBack(i);
-  }
+	
+	function moveBack(i){
+		for (; i > 0 && arr[i - 1] > arr[i]; i--) {
+			var t = arr[i];
+			arr[i] = arr[i - 1];
+			arr[i - 1] = t;
+		}
+	}
+	for (i = 1; i < arr.length; i++){
+		if (arr[i - 1] > arr[i])
+			moveBack(i);
+	}
   return arr;
 }
